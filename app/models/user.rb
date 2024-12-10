67b86e21866f13fce8b95d212616f2299840e5aa
Cyrill Semah <cyrillsemah@gmail.com>
2024-12-10 21:02:14 +0100

    Amélioration des modèles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :cars, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  has_many :booked_cars, through: :bookings, source: :car
+
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
 end
