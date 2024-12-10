6dc17292318fa67beb1363835675f88c54aaef95
Cyrill Semah <cyrillsemah@gmail.com>
2024-12-11 00:17:38 +0100

    front start

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,10 @@
+//= link_tree ../images
+//= link_tree ../builds
+//= link application.css
+//= link application.js
+//= link bootstrap.min.js
+//= link popper.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/map_controller.js
+//= link controllers/index.js
--- db/migrate/20241210210103_drop_and_recreate_booking_table.rb
+++ db/migrate/20241210210103_drop_and_recreate_booking_table.rb
@@ -1,11 +1,12 @@
-class DropAndRecreateBookingTable < ActiveRecord::Migration[8.0]
+class DropAndRecreateBookingTable < ActiveRecord::Migration[7.1]
   def up
     drop_table :bookings, if_exists: true
 
     create_table :bookings do |t|
       t.date :start_date
       t.date :end_date
-      t.integer :number_of_people
+      t.decimal :total_price
+      t.string :status
       t.references :user, null: false, foreign_key: true
       t.references :car, null: false, foreign_key: true
 
