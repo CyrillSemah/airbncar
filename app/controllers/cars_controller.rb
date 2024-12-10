8fa79e8b4ce5d83aaab27865654ad5217101a278
Cyrill Semah <cyrillsemah@gmail.com>
2024-12-10 21:47:14 +0100

    fixed

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,52 @@
 class CarsController < ApplicationController
+  before_action :set_car, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @cars = Car.all
+  end
+
+  def show
+    @booking = Booking.new
+    @bookings = @car.bookings
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = current_user.cars.build(car_params)
+    if @car.save
+      redirect_to @car, notice: "La voiture a été ajoutée avec succès."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @car.update(car_params)
+      redirect_to @car, notice: "Les détails de la voiture ont été mis à jour avec succès."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+    @car.destroy
+    redirect_to cars_path, notice: "La voiture a été supprimée avec succès."
+  end
+
+  private
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  def car_params
+    params.require(:car).permit(:title, :description, :location, :capacity, :size, :daily_price)
+  end
 end
--- test/controllers/cars_controller_test.rb
+++ test/controllers/cars_controller_test.rb
@@ -1,7 +1,63 @@
 require "test_helper"
 
 class CarsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+    @car = cars(:one)
+    sign_in @user
+  end
+
+  test "should get index" do
+    get cars_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_car_url
+    assert_response :success
+  end
+
+  test "should create car" do
+    assert_difference('Car.count') do
+      post cars_url, params: {
+        car: {
+          title: "Test Car",
+          description: "A test car description",
+          location: "Test Location",
+          capacity: 4,
+          size: "Medium",
+          daily_price: 50.00
+        }
+      }
+    end
+    assert_redirected_to car_url(Car.last)
+  end
+
+  test "should show car" do
+    get car_url(@car)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_car_url(@car)
+    assert_response :success
+  end
+
+  test "should update car" do
+    patch car_url(@car), params: {
+      car: {
+        title: "Updated Title"
+      }
+    }
+    assert_redirected_to car_url(@car)
+    @car.reload
+    assert_equal "Updated Title", @car.title
+  end
+
+  test "should destroy car" do
+    assert_difference('Car.count', -1) do
+      delete car_url(@car)
+    end
+    assert_redirected_to cars_url
+  end
 end
